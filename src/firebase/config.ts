5536aae941caaba51014483ae18d83b217f826cc
Pietro Janeri <pietrojaneri2@gmail.com>
2025-10-30 20:29:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7150330332-1b378",
+  "appId": "1:74151965141:web:5b283f4133e6c375d58d5c",
+  "apiKey": "AIzaSyCjRAEabvfdBYsiNyVzpRwkou219nW-y5I",
+  "authDomain": "studio-7150330332-1b378.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "74151965141"
+};
