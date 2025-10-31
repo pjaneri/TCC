0c9fcd8a563982011b711cab6c4ad8ac52b860f6
Pietro Janeri <pietrojaneri2@gmail.com>
2025-10-31 18:27:12 +0000

    eu nao consigo abrir esse arquivo, nao tem como eu colocar a chave em ou

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import config from '../../config.json';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: config.GEMINI_API_KEY})],
   model: 'googleai/gemini-2.5-flash',
 });
