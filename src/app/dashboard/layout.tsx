57a1a25e22f0d53a13d88ffc5141765c81454eef
Pietro Janeri <pietrojaneri2@gmail.com>
2025-10-31 17:42:01 +0000

    pode tirar isso ta dando, e vc tem ideias de comom aumentar a segurança

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -10,7 +10,6 @@ import {
   UserCircle,
   Recycle,
   BarChart,
-  PieChart,
 } from "lucide-react";
 import { ReactNode, useEffect } from "react";
 import { getAuth, signOut } from "firebase/auth";
@@ -35,7 +34,6 @@ import { ThemeToggle } from "@/components/theme-toggle";
 const navItems = [
   { href: "/dashboard", icon: LayoutDashboard, label: "Visão Geral" },
   { href: "/dashboard/log", icon: PlusCircle, label: "Registrar Reciclagem" },
-  { href: "/dashboard/statistics", icon: PieChart, label: "Estatísticas" },
   { href: "/dashboard/rankings", icon: BarChart, label: "Rankings" },
   { href: "/dashboard/rewards", icon: Trophy, label: "Resgatar Prêmios" },
   { href: "/dashboard/profile", icon: UserCircle, label: "Perfil" },
