4768fbca3f404f518e9eaedcfb366f442700501c
Pietro Janeri <pietrojaneri2@gmail.com>
2025-11-07 13:07:21 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -33,6 +33,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { useToast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
